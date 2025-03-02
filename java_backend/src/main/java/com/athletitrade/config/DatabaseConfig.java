5b6157b4dfca2d8b3fb1c00339f8c9798aaee203
JTSmullen <158128671+JTSmullen@users.noreply.github.com>
2025-03-02 00:38:33 -0500

    added dependencies to pom.xml, created a base database schema, Created the java config file of the database.

--- java_backend/src/main/java/com/athletitrade/config/DatabaseConfig.java
+++ java_backend/src/main/java/com/athletitrade/config/DatabaseConfig.java
@@ -1,8 +1,20 @@
 package com.athletitrade.config;
 
+import javax.sql.DataSource;
 
-/*
-    TODO: Configure the database connection
- */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
 public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("database/schema.sql") // Loads the schema on launch
+                .build();
+    }
 }
